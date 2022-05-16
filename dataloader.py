7a7ad4888d16f03c5b35b297c6cd002b1e0baa89
sangohe <santiagogomez.h@outlook.com>
2022-05-16 11:49:56 -0500

    ✏️ Dataloader now correctly imports the preprocessing module

--- dataloader.py
+++ dataloader.py
@@ -3,15 +3,16 @@ from torch.utils.data import Dataset
 import os
 from typing import List, Optional, Any
 
-from utils import read_image, get_label_from_path, get_patient_id_from_path, get_eye_laterality_from_path
+from utils.preprocessing import (
+    read_image,
+    get_label_from_path,
+    get_patient_id_from_path,
+    get_eye_laterality_from_path,
+)
+
 
 class EyesDataset(Dataset):
-
-    def __init__(
-        self, 
-        patient_dirs: List[str],
-        transform: Optional[Any] = None
-    ):
+    def __init__(self, patient_dirs: List[str], transform: Optional[Any] = None):
 
         self.patient_dirs = patient_dirs
         self.transform = transform
@@ -19,9 +20,11 @@ class EyesDataset(Dataset):
         # Create a list with all the frame paths.
         self.frame_paths = []
         for patient_dir in patient_dirs:
-            patient_frame_paths = [os.path.join(patient_dir, p) for p in os.listdir(patient_dir)]
+            patient_frame_paths = [
+                os.path.join(patient_dir, p) for p in os.listdir(patient_dir)
+            ]
             self.frame_paths.extend(patient_frame_paths)
-    
+
     def __len__(self):
         return len(self.frame_paths)
 
